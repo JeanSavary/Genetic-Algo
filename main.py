1edd45cfce51c34e6585d68e152097d913e6ecd9
JeanSvy <jeansvy@gmail.com>
2019-03-18 18:50:40 +0100

    reduce mutation rate & inclute elite population in the wheel selection

--- main.py
+++ main.py
@@ -26,20 +26,20 @@ def selection(ranked_population, elite_size):
     '''
 
     selection_results = [elem[0] for elem in ranked_population[:elite_size]] # We directly select our 'elite' population
-    fitness_list = [elem[1] for elem in ranked_population[elite_size:]] # Then we'll perform the "roulette wheel selection"
+    fitness_list = [elem[1] for elem in ranked_population] # Then we'll perform the "roulette wheel selection"
 
     df = pd.DataFrame(fitness_list, columns = ['fitness'])
     df['cum_sum'] = df.cumsum()
     df['cum_%'] = 100 * df['cum_sum'] / df['fitness'].sum()
    
-    for iteration in range(len(fitness_list)):
+    for iteration in range(len(fitness_list) - elite_size):
 
         threshold = uniform(0, 100)
         for index, _ in enumerate(fitness_list) : 
 
             if threshold <= df.iloc[index]['cum_%'] : 
 
-                chosen_route = ranked_population[elite_size + index][0]
+                chosen_route = ranked_population[index][0]
                 selection_results.append(chosen_route)
                 break
 
